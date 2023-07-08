74827ecba2acf9c911a13a6133eaa42f25bd2e2d
Prathmesh Chavan <96901316+prathmchavan@users.noreply.github.com>
2023-07-09 01:59:03 +0530

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    bracketSpacing: true,
+
+    singleQuote: true,
+
+    tabwidth: 2,
+    useTabs: false,
+    trailingComma: "none",
+    semi: false,
+
+
+
+};
--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context'
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Current Wheather</Text>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor:'purple'
+  },
+  text: {
+    fontSize: 25,
+    fontWeight: '500',
   },
 });
+
+export default App;
