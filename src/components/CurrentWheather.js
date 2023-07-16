dee3f0b3e6db94c6b33e793e1c50bea593be0fd9
Prathmesh Chavan <96901316+prathmchavan@users.noreply.github.com>
2023-07-16 15:13:58 +0530

    listing the wheathers

--- src/components/CurrentWheather.js
+++ src/components/CurrentWheather.js
@@ -1,42 +1,25 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Feather } from '@expo/vector-icons';
 
-const CurrentWheather = () => {
+const CurrentWeather = () => {
   return (
     <SafeAreaView style={styles.wrapper}>
       <View style={styles.container}>
-      <Feather name="sun" size={100
-      } color="black" />
-        <Text>
-          current wheather
-        </Text>
-        <Text style={styles.temp}>
-          6
-        </Text>
-        <Text style={styles.feel}>
-          feels like 5
-        </Text>
+        <Feather name="sun" size={100} color="black" />
+        <Text>current weather</Text>
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feel}>feels like 5</Text>
         <View style={styles.hlwrapper}>
-          <Text style={styles.hl}>
-            High: 8
-          </Text>
-          <Text style={styles.hl}> Low: 6</Text>
+          <Text style={styles.hl}>High: 8</Text>
+          <Text style={styles.hl}>Low: 6</Text>
         </View>
-
-
-
       </View>
-
-      
       <View style={styles.bodywrapper}>
-        <Text style={styles.description}>
-          It is sunny outside
-        </Text>
-        <Text style={styles.message}>
-          wheather is clear wear white cloths
-        </Text>
+        <Text style={styles.description}>It is sunny outside</Text>
+        <Text style={styles.message}>Weather is clear, wear white clothes</Text>
       </View>
     </SafeAreaView>
   );
@@ -46,53 +29,43 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent:'center'
-
-
+    justifyContent: 'center',
   },
   text: {
     fontSize: 25,
     fontWeight: '500',
   },
   wrapper: {
-
     backgroundColor: '#E6FFFD',
-    flex: 1
+    flex: 1,
   },
   temp: {
     color: 'black',
-    fontSize: 48
+    fontSize: 48,
   },
   feel: {
     color: 'black',
-    fontSize: 30
+    fontSize: 30,
   },
   hlwrapper: {
-    flexDirection: 'row'
-  }
-  ,
+    flexDirection: 'row',
+  },
   hl: {
     color: 'black',
-    fontSize: 20
+    fontSize: 20,
   },
   bodywrapper: {
-    // flexGrow: 1,
-    // alignSelf: 'stretch',
     justifyContent: 'flex-end',
     alignItems: 'flex-start',
-    paddingLeft:25,
-    marginBottom:40
+    paddingLeft: 25,
+    marginBottom: 40,
   },
   description: {
-    fontSize: 48
+    fontSize: 48,
   },
   message: {
-    fontSize: 30
-  }
-
-
-
-
+    fontSize: 30,
+  },
 });
 
-export default CurrentWheather;
+export default CurrentWeather;
